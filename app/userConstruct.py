ca43acb1e1a19dbf05331115785ee2de558c15ae
Jonathan Tsai <modopo@gmail.com>
2017-12-05 23:47:07 -0800

    create_user method initialized

--- app/userConstruct.py
+++ app/userConstruct.py
@@ -1,4 +1,5 @@
 from app.db_setup import db_session, User
 
 def create_user(login_session):
-    return False
+    newUser = User(name = login_session['username'], email = login_session[
+        'email'], picture = login_session['picture'])
