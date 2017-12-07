54412f67bff15bc399fa68f4dd8715c783a80e40
Jonathan Tsai <modopo@gmail.com>
2017-12-06 17:07:48 -0800

    Decorater login verification implemented

---
+++ app/loginVerification.py
@@ -0,0 +1,12 @@
+from functools import wraps
+from flask import redirect
+from flask import session as login_session
+
+def login_required(function):
+    @wraps(function)
+    def wrapper(*args, **kwargs):
+        if 'username' not in login_session:
+            return redirect('/login')
+        else:
+            return function(*args, **kwargs)
+    return wrapper
