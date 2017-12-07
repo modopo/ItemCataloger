24697bafb801dca8a614eb09e2dfc7de8d83f173
Jonathan Tsai <modopo@gmail.com>
2017-12-06 16:49:46 -0800

    JSON API endpoint created

--- app/views/index.py
+++ app/views/index.py
@@ -5,7 +5,7 @@ from app.db_setup import db_session, Categories, Items
 index = Blueprint('home', __name__)
 
 @index.route('/')
-@index.route('/catalogue')
+@index.route('/category')
 def index():
     categories = db_session.query(Categories).order_by(asc(Categories.name))
     items = db_session.query(Items).order_by(desc(Items))
